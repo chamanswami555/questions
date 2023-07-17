4736766a994620d1dba97363ab67187865b6a02f
Chaman Swami <114087202+chamanswami555@users.noreply.github.com>
2023-07-17 18:10:47 +0530

    question
    generate 100 random numbers and check them that they are odd or prime and then make their separate array.

---
+++ program1.c
@@ -0,0 +1,95 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<stdbool.h>
+#include<math.h>
+
+bool Prime(int);
+bool Odd(int);
+
+int main() 
+{	
+    int rn[100];
+    int on[100];
+    int pn[100];
+    int oc=0;
+    int pc=0;
+    int i;
+    
+    printf("\n\t100 Random Numbers :-\n");
+    
+    for(i=0;i<100;i++)
+	{		
+        rn[i]=rand()%50;   // Generate random numbers from 1 to 50
+        
+        printf("%d ",rn[i]);
+
+        if(Odd(rn[i]))
+		{
+            on[oc]=rn[i];
+            oc++;
+        }
+
+        if(Prime(rn[i]))
+		{
+            pn[pc]=rn[i];
+            pc++;
+        }
+    }
+
+    printf("\n\n\tOdd Numbers :-\n");
+    
+    for(i=0;i<oc;i++)
+	{
+        printf("%d ",on[i]);
+    }
+
+    printf("\n\n\tPrime Numbers :-\n");
+    
+    for(i=0;i<pc;i++)
+	{
+        printf("%d ",pn[i]);
+    }
+
+    return 0;
+    
+}
+
+
+
+bool Prime(int num)
+{
+	
+	int i;
+	
+    if(num<=1)
+    {
+    	return false;
+	}
+	
+	for(i=2;i<=sqrt(num);i++)
+	{
+        if (num%i==0)
+        return false;
+    }
+    
+    return true;
+    
+}
+
+
+bool Odd(int num)
+{
+	
+	if(num%2!=0)
+	{
+		return true;
+	}
+	
+	else
+	{
+		return false;
+	}
+	
+}
+
+//~chamanswami
---
+++ program1pro.c
@@ -0,0 +1,103 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<stdbool.h>
+#include<math.h>
+
+bool Prime(int);
+bool Odd(int);
+
+int main()
+{
+    int *rn=(int*)malloc(100*sizeof(int));
+    int *on=NULL;
+    int *pn=NULL;
+    int oc=0;
+    int pc=0;
+    int i;
+    
+    printf("\n\t100 Random Numbers :-\n");
+
+    for(i=0;i<100;i++)
+	{		
+        rn[i]=rand()%50;  // Generate random numbers from 1 to 1000
+
+        printf("%d ",rn[i]);
+
+        if(Odd(rn[i]))
+		{
+            oc++;
+            on=(int*)realloc(on,oc*sizeof(int));
+            on[oc-1]=rn[i];
+        }
+
+        if(Prime(rn[i]))
+		{
+            pc++;
+            pn=(int*)realloc(pn,pc*sizeof(int));
+            pn[pc-1]=rn[i];
+        }
+    }
+
+    printf("\n\n\tOdd Numbers :-\n");
+    
+    for (i=0;i<oc;i++)
+	{
+        printf("%d ",*(on+i));
+    }
+
+    printf("\n\n\tPrime Numbers:\n");
+    
+    for (i=0;i<pc;i++)
+	{
+        printf("%d ",*(pn+i));
+    }
+
+    // Free dynamically allocated memory
+    
+    free(rn);
+    free(on);
+    free(pn);
+
+    return 0;
+    
+}
+
+
+
+bool Prime(int num)
+{
+	
+	int i;
+	
+    if(num<=1)
+    {
+    	return false;
+	}
+	
+	for(i=2;i<=sqrt(num);i++)
+	{
+        if (num%i==0)
+        return false;
+    }
+    
+    return true;
+    
+}
+
+
+bool Odd(int num)
+{
+	
+	if(num%2!=0)
+	{
+		return true;
+	}
+	
+	else
+	{
+		return false;
+	}
+	
+}
+
+//~chamanswami
